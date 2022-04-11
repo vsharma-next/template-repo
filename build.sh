8176ce777232e27155a968ffa6b06d3766c859e9
Varun Sharma <vsharma.next@gmail.com>
2022-04-11 17:31:23 +0200

    replaced git with https

--- build.sh
+++ build.sh
@@ -3,23 +3,18 @@
 set -e # if a command fails it stops the execution
 set -u # script fails if trying to access to an undefined variable
 
-
 mkdir output
 
-#find . ! -regex '.*/output' ! -regex '.' -not -path '*/.*' -exec cp -r '{}' output \;
-
-#find . -not -path './output' -not -path '.' -not -path '*/.*'
-
 find . -maxdepth 1 -not -path './output' -not -path '.' -not -path '*/.*' -exec cp -r '{}' output/ \;
-
 find ./output -type f ! -name "*.md" -and ! -name "*.ipynb" -delete
 
 cd output/
 
 path="${1}"
-echo $path
+pathA=$(echo "$path" | sed "s/git/https/")
+echo $pathA
 
-sed -i "s~GITHUB-URL~$path~g" README.md
+sed -i "s~GITHUB-URL~$pathA~g" README.md
 
 more README.md
 
