c9b6ac8a80b32bb6abc5ca9ff197d703da522929
Varun Sharma <vsharma.next@gmail.com>
2022-04-11 17:10:25 +0200

    added some changes make build better

--- build.sh
+++ build.sh
@@ -6,7 +6,11 @@ set -u # script fails if trying to access to an undefined variable
 
 mkdir output
 
-find . ! -regex '.*/output' ! -regex '.' -not -path '*/.*' -exec cp -r '{}' output \;
+#find . ! -regex '.*/output' ! -regex '.' -not -path '*/.*' -exec cp -r '{}' output \;
+
+#find . -not -path './output' -not -path '.' -not -path '*/.*'
+
+find . -maxdepth 1 -not -path './output' -not -path '.' -not -path '*/.*' -exec cp -r '{}' output/ \;
 
 find ./output -type f ! -name "*.md" -and ! -name "*.ipynb" -delete
 
